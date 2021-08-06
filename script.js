a45e9dc1d0fdc806511023850af1d4f91f345861
Natasha <njohnson533@gmail.com>
2021-08-05 20:34:22 -0400

    commented out caret and dropdown event listeners since they havent been implemented yet in the redesign to fix js error, hamburger button is now working

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const caret = document.getElementById("caret");
-const caret1 = document.getElementById("caret1");
-const caretRotate1 = document.getElementById("caret-1");
-const caretRotate2 = document.getElementById("caret-2");
-const dropdown = document.getElementById("dropdown-content");
-const dropdown1 = document.getElementById("dropdown-content1");
+// const caret = document.getElementById("caret");
+// const caret1 = document.getElementById("caret1");
+// const caretRotate1 = document.getElementById("caret-1");
+// const caretRotate2 = document.getElementById("caret-2");
+// const dropdown = document.getElementById("dropdown-content");
+// const dropdown1 = document.getElementById("dropdown-content1");
 const hamburger = document.getElementById("hamburger");
 const hamburgerBtn = document.getElementById("hamburger-btn");
-const navLinks = document.getElementById("nav-links");
+// const navLinks = document.getElementById("nav-links");
 
-caret.addEventListener("click", () => {
-    dropdown.classList.toggle("open");
-    caretRotate1.classList.toggle("caret-open");
-});
+// caret.addEventListener("click", () => {
+//     dropdown.classList.toggle("open");
+//     caretRotate1.classList.toggle("caret-open");
+// });
 
-caret1.addEventListener("click", () => {
-    dropdown1.classList.toggle("open");
-    caretRotate2.classList.toggle("caret-open");
-});
+// caret1.addEventListener("click", () => {
+//     dropdown1.classList.toggle("open");
+//     caretRotate2.classList.toggle("caret-open");
+// });
 
 hamburger.addEventListener("click", () => {
     hamburgerBtn.classList.toggle("open-hamburger");
