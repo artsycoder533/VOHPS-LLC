56403624d77280e0d5884afd5a7cf5fec4f2041b
Natasha <njohnson533@gmail.com>
2021-08-20 14:59:30 -0400

    added all services section, created a tab for each service with transitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,66 @@ const hamburger = document.getElementById("hamburger");
 const hamburgerBtn = document.getElementById("hamburger-btn");
 const navLinks = document.getElementById("nav-links");
 
+
+// target all tabs
+const tabs = document.querySelectorAll(".btn-services");
+
+// parent container
+const services = document.querySelector(".all-services");
+
+// target all content
+const contents = document.querySelectorAll(".content");
+
+services.addEventListener('click', function (e) {
+	const id = e.target.dataset.id;
+	if (id) {
+		// remove active tab
+		tabs.forEach(function (tab) {
+			tab.classList.remove("active");
+			e.target.classList.add("active");
+		});
+		
+		// hide other services
+		    contents.forEach(function (service) {
+			service.classList.remove("show-content");
+			});
+		
+		//show individual service
+		const element = document.getElementById(id);
+		element.classList.add("show-content");
+	}
+});
+
+// tabs.forEach(function (tab) {
+// 	const btn = tab.querySelector(".btn-services");
+// 	btn.addEventListener('click', function () {
+// 		services.forEach(function (service) {
+// 			if (service !== tab) {
+// 				service.classList.remove("show-content");
+// 				// tab.classList.add("hide-content");
+// 			}
+// 		})
+		
+// 	})
+// })
+
+
 // target all questions
 const questions = document.querySelectorAll(".question");
 
+questions.forEach(function (question) {
+	const btn = question.querySelector(".faq-btn");
+	btn.addEventListener("click", function () {
+		questions.forEach(function (item) {
+			if (item !== question) {
+				item.classList.remove("show");
+			}
+		});
+		question.classList.toggle("show");
+	});
+});
+
+// navbar
 caret.addEventListener("click", () => {
 	dropdown.classList.toggle("open");
 	caretRotate1.classList.toggle("caret-open");
@@ -26,14 +83,3 @@ hamburger.addEventListener("click", () => {
 	navLinks.classList.toggle("show-links");
 });
 
-questions.forEach(function (question) {
-	const btn = question.querySelector(".faq-btn");
-	btn.addEventListener('click', function () {
-		questions.forEach(function (item) {
-			if (item !== question) {
-				item.classList.remove("show");
-			}
-		});
-		question.classList.toggle("show");
-	});
-});
