2869f5433cb3e66d1c140dbb66c8a1f16b0515d7
Natasha <njohnson533@gmail.com>
2021-08-21 19:52:35 -0400

    added back the slider to the services section

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ const navLinks = document.getElementById("nav-links");
 const tabs = document.querySelectorAll(".btn-services");
 
 // parent container
-const services = document.querySelector(".all-services-section");
+const services = document.querySelector(".all-services");
 
 // target all content
 const contents = document.querySelectorAll(".content");
