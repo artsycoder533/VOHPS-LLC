a6c28914fbaaea6dc9ae20e78fad041331faca2f
Natasha <njohnson533@gmail.com>
2021-10-25 20:47:24 -0400

    removed commented out code in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,28 @@
-//const caret = document.getElementById("caret");
-// const caret1 = document.getElementById("caret1");
-//const caretRotate1 = document.getElementById("caret-1");
-// const caretRotate2 = document.getElementById("caret-2");
-//const dropdown = document.getElementById("dropdown-content");
-//const dropdown1 = document.getElementById("dropdown-content1");
+
 const hamburger = document.getElementById("hamburger");
 const hamburgerBtn = document.getElementById("hamburger-btn");
 const navLinks = document.getElementById("nav-links");
-// target all tabs
-const tabs = document.querySelectorAll(".btn-services");
-
-// parent container
-// const servicesContainer = document.querySelector(".all-services");
-//console.log(servicesContainer);
-
-// target all content
+// const tabs = document.querySelectorAll(".btn-services");
 const contents = document.querySelectorAll(".content");
+const questions = document.querySelectorAll(".question");
+const scrollBtn = document.getElementById("scroll");
 
-
-
-// navbar
-// caret.addEventListener("click", () => {
-// 	dropdown.classList.toggle("open");
-// 	caretRotate1.classList.toggle("caret-open");
-// });
-
-//close navbar when link clicked
 navLinks.addEventListener("click", () => {
 	hamburgerBtn.classList.toggle("open-hamburger");
 	navLinks.classList.remove("show-links");
 });
 
-// caret1.addEventListener("click", () => {
-// 	dropdown1.classList.toggle("open");
-// 	caretRotate2.classList.toggle("caret-open");
-// });
 
 hamburger.addEventListener("click", () => {
-	//console.log("clicked");
 	hamburgerBtn.classList.toggle("open-hamburger");
 	navLinks.classList.toggle("show-links");
 });
 
-//sticky header
 window.addEventListener("scroll", () => {
     const header = document.querySelector(".header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-
-// target all questions
-const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
 	const btn = question.querySelector(".faq-btn");
@@ -64,24 +36,18 @@ questions.forEach(function (question) {
 	});
 });
 
+// tabs.forEach(function (tab) {
+// 	const btn = document.querySelector(".btn-services");
+// 	btn.addEventListener("click", function () {
+//         contents.forEach(function (service) {
+// 			if (service !== tab) {
+// 				service.classList.remove("show-content");
+// 			}
+// 		});
+// 	});
+// });
 
 
-
-tabs.forEach(function (tab) {
-	const btn = document.querySelector(".btn-services");
-	//const btn = tab.querySelector(".btn-services");
-	btn.addEventListener("click", function () {
-        contents.forEach(function (service) {
-            //console.log(services);
-			if (service !== tab) {
-				service.classList.remove("show-content");
-				// tab.classList.add("hide-content");
-			}
-		});
-	});
-});
-
-const scrollBtn = document.getElementById("scroll");
 
 window.onscroll = function () {
 	if (document.documentElement.scrollTop > 70) {
@@ -94,25 +60,5 @@ window.onscroll = function () {
 scrollBtn.addEventListener("click", () => {
 	document.documentElement.scrollTop = 0;
 });
-
-// servicesContainer.addEventListener("click", function (e) {
-// 	const id = e.target.dataset.id;
-// 	if (id) {
-// 		// remove active tab
-// 		tabs.forEach(function (tab) {
-// 			tab.classList.remove("active");
-// 			e.target.classList.add("active");
-// 		});
-
-// 		// hide other services
-// 		contents.forEach(function (service) {
-// 			service.classList.remove("show-content");
-// 		});
-
-// 		//show individual service
-// 		const element = document.getElementById(id);
-// 		element.classList.add("show-content");
-// 	}
-// });
 
 
